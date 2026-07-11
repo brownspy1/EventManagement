e96169550d8499512f707857a113a47fbe8a0c62
brownspy1 <brownspy02@gmail.com>
2026-07-12 05:38:54 +0600

    Add initial models, forms, and templates for event management

--- event/models.py
+++ event/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+'''How may model Work'''
+'''
+
+Category--(1:M)-->Event--(M:M)-->Participant
+
+'''
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=250,null=False,blank=False)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.TextField()
+    category = models.ForeignKey('Category',on_delete=models.CASCADE,default=1,related_name='events')
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+class Participant(models.Model):
+    name = models.CharField(max_length=250,null=False,blank=False)
+    email = models.EmailField(unique=True)
+    registered_events = models.ManyToManyField(Event,related_name='participants',blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=250,null=False,blank=False)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+
--- event/views.py
+++ event/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from event.forms import . 
 # Create your views here.
 
 def home(request):
     return render(request,'Dashbord.html')
+
+def addCategory(request):
+    
+    pass  
