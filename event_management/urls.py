8ed08796b9577e8f70939ccd96843e167713f632
brownspy1 <brownspy02@gmail.com>
2026-07-23 06:01:37 +0600

    Make debug toolbar urls conditional on settings.DEBUG

--- event_management/urls.py
+++ event_management/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from event import urls
-from debug_toolbar.toolbar import debug_toolbar_urls
-
+from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('event.urls'))
-]+ debug_toolbar_urls()
+    path('', include('event.urls'))
+]
+
+if settings.DEBUG:
+    from debug_toolbar.toolbar import debug_toolbar_urls
+    urlpatterns += debug_toolbar_urls()
+
